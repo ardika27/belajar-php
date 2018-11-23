89a3faace7a57ca98a5aa9d7fc0e57544123b09f
boyhendrawan <boyhendrawanpurba@gmail.com>
2018-11-23 15:35:58 +0700

    boy

---
+++ float2.php
@@ -0,0 +1,11 @@
+<?php
+
+//float adalah bilangan int yang bernilai desimal
+
+$float=0.25;
+$float2=5;
+
+$float3 = $float * $float2;
+echo "bilangan float".$float;
+echo "Hasil".$float3;
+ ?>
---
+++ int2.php
@@ -0,0 +1,9 @@
+<?php
+$umur=16;
+   $harga=15000;
+   
+echo"harga baju saya".$harga;
+
+echo "ini Umur saya".$umur; 
+
+?>
